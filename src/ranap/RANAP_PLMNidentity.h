c89c2a6e6a1a2a34413fd186670753ae1574ce89
Harald Welte <laforge@gnumonks.org>
2015-12-24 14:07:05 +0100

    RANAP: Replace the last remainng TBCD-STRING with OCTET STRING
    in order to work around a bug in asn1c.  When we keep the original
    TBCD-STRING, the APER-encoded PLMNidentity always has an extra leading
    length byte that the decoder doesn't expect.

--- src/ranap/RANAP_PLMNidentity.h
+++ src/ranap/RANAP_PLMNidentity.h
@@ -12,14 +12,14 @@
 #include <asn_application.h>
 
 /* Including external dependencies */
-#include "RANAP_TBCD-STRING.h"
+#include <OCTET_STRING.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /* RANAP_PLMNidentity */
-typedef RANAP_TBCD_STRING_t	 RANAP_PLMNidentity_t;
+typedef OCTET_STRING_t	 RANAP_PLMNidentity_t;
 
 /* Implementation */
 extern asn_TYPE_descriptor_t asn_DEF_RANAP_PLMNidentity;
