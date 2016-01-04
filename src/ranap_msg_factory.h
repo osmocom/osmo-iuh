c04a96c476e8ef87932b93265fb197b9ff45b38a
Neels Hofmeyr <neels@hofmeyr.de>
2016-01-04 18:03:31 +0100

    move asn1 headers to include/ dir (5/5)
    Do all remaining changes necessary for a working build.
    
    Add Makefile.am files in include/... subdirs.
    
    Remove noinst_HEADERS directives from src/*/Makefile.am, but keep the headers
    list to feed to move-asn1-header-files.sh.
    
    Adjust all #includes in src/*_common.h and elsewhere. In hnbap_common.h,
    separate the ASN.1 "primitive" headers from the others, and include them
    without a subdir path, as before.

--- src/ranap_msg_factory.h
+++ src/ranap_msg_factory.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #include <stdint.h>
-#include "ranap/RANAP_Cause.h"
-#include "ranap/RANAP_CN-DomainIndicator.h"
-#include "ranap/RANAP_GlobalRNC-ID.h"
+#include <osmocom/osmo-iuh/ranap/RANAP_Cause.h>
+#include <osmocom/osmo-iuh/ranap/RANAP_CN-DomainIndicator.h>
+#include <osmocom/osmo-iuh/ranap/RANAP_GlobalRNC-ID.h>
 
 /*! \brief generate RANAP DIRECT TRANSFER message */
 struct msgb *ranap_new_msg_dt(uint8_t sapi, const uint8_t *nas, unsigned int nas_len);
