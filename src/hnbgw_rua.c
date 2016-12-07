osmocom/osmo-iuh

commit 2b5021fd8d445f0ca4f28e0ea71a19cd52724e57

hnbgw: rua rx: fix rc and log unhandled Private Msg
Fixes: CID#57949
Change-Id: I822888a3cf450e2787fc352e0352aed92236ddb7

src/hnbgw_rua.c
@@ -475,6 +475,9 @@ static int rua_rx_initiating_msg(struct msgb *msg, RUA_InitiatingMessage_t *imsg
 		rc = rua_rx_init_err_ind(msg, &imsg->value);
 		break;
 	case RUA_ProcedureCode_id_privateMessage:
+		LOGP(DRUA, LOGL_NOTICE,
+		     "Unhandled: RUA Initiating Msg: Private Msg\n");
+		rc = 0;
 		break;
 	default:
 		LOGP(DRUA, LOGL_NOTICE, "Unknown RUA Procedure %u\n",

commit 32b821005e69813cea335f6b88568db6144111b9

log: fix number format for domain indicator
CN_DomainIndicator_t is a long, so use %l format.
Error was introduced in recent 2b5021fd8d445f0ca4f28e0ea71a19cd52724e57.

Fixes: CID#157119
Change-Id: I22f49d45bc87897beb24a6b1eee6c83da3e57b0f

src/hnbgw_rua.c
@@ -348,7 +348,7 @@ static int rua_rx_init_disconnect(struct msgb *msg, ANY_t *in)
 		cn = hnb->gw->cnlink_ps;
 		break;
 	default:
-		LOGP(DRUA, LOGL_ERROR, "Invalid CN_DomainIndicator: %u\n",
+		LOGP(DRUA, LOGL_ERROR, "Invalid CN_DomainIndicator: %l\n",
 		     ies.cN_DomainIndicator);
 		rc = -1;
 		goto error_free;
@@ -394,7 +394,7 @@ static int rua_rx_init_dt(struct msgb *msg, ANY_t *in)
 		cn = hnb->gw->cnlink_ps;
 		break;
 	default:
-		LOGP(DRUA, LOGL_ERROR, "Invalid CN_DomainIndicator: %u\n",
+		LOGP(DRUA, LOGL_ERROR, "Invalid CN_DomainIndicator: %l\n",
 		     ies.cN_DomainIndicator);
 		rc = -1;
 		goto error_free;