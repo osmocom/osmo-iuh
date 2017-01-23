9e6294c4d88442b20c75e39d387b4ceca1f77a6d
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2017-01-23 16:39:24 +0100

    cs RAB: toggle Pre_emptionVulnerability
    Change-Id: I0f4d5a5e56bb6a3a3dfd6db8292d30f55fccdbc1

--- src/ranap_msg_factory.c
+++ src/ranap_msg_factory.c
@@ -581,7 +581,7 @@ static RANAP_RAB_Parameters_t *new_rab_par_voice(void)
 	ASN_SEQUENCE_ADD(&rab->sDU_Parameters, sdui);
 
 	rab->transferDelay = new_long(80);
-	rab->allocationOrRetentionPriority = new_alloc_ret_prio(RANAP_PriorityLevel_lowest, 0, 1, 0);
+	rab->allocationOrRetentionPriority = new_alloc_ret_prio(RANAP_PriorityLevel_lowest, 0, 0, 0);
 
 	rab->sourceStatisticsDescriptor = new_long(RANAP_SourceStatisticsDescriptor_speech);
 
