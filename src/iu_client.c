cf5fbea78f2f7316e7cbe91f32cb79b4a02abd75
Neels Hofmeyr <neels@hofmeyr.de>
2018-03-21 15:50:41 +0100

    cosmetic: use osmo_plmn_id instead of mcc,mnc
    It's cosmetic since the MCC and MNC in the PLMN aren't actually used,
    currently. It makes sense to do it properly anyway, while I'm still in the
    3-digit MNC topic, for the future.
    
    Change-Id: I29ebcddd45fe3079f8883589a83cc7216a535475

--- src/iu_client.c
+++ src/iu_client.c
@@ -42,8 +42,7 @@
  * PLMN identity is a BCD representation of the MCC and MNC.
  * See iu_grnc_id_parse(). */
 struct iu_grnc_id {
-	uint16_t mcc;
-	uint16_t mnc;
+	struct osmo_plmn_id plmn;
 	uint16_t rnc_id;
 };
 
@@ -310,8 +309,7 @@ static int iu_grnc_id_parse(struct iu_grnc_id *dst, struct RANAP_GlobalRNC_ID *s
 		       " should be 3, is %d\n", src->pLMNidentity.size);
 		return -1;
 	}
-	gsm48_mcc_mnc_from_bcd(&src->pLMNidentity.buf[0],
-			       &dst->mcc, &dst->mnc);
+	osmo_plmn_from_bcd(&src->pLMNidentity.buf[0], &dst->plmn);
 	dst->rnc_id = (uint16_t)src->rNC_ID;
 	return 0;
 }
