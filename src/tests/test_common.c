a94752e684ad30c59b4da66565a83a62edd569ab
Pau Espin Pedrol <pespin@sysmocom.de>
2021-02-19 13:19:01 +0100

    tests: Replace deprecated API log_set_print_filename
    Change-Id: Ic53a0a9afb8038759bb3ab1d4c5970f23632e84e

--- src/tests/test_common.c
+++ src/tests/test_common.c
@@ -84,7 +84,7 @@ int test_common_init(void)
 
 	ranap_set_log_area(DRANAP);
 
-	log_set_print_filename(osmo_stderr_target, 0);
+	log_set_print_filename2(osmo_stderr_target, LOG_FILENAME_NONE);
 	log_set_use_color(osmo_stderr_target, 0);
 	log_set_print_category(osmo_stderr_target, 0);
 	log_set_print_category_hex(osmo_stderr_target, 0);
