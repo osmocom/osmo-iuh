osmocom/osmo-iuh

commit adec82f3b1c725e8a70d327b9592940fbdeaaff7

iu_client: Prevent crash if msgb passed to ranap_iu_tx has no dst
Related: OS#5398
Change-Id: I64beecd2c492d6312f60a1cf016ef022cda5252d

src/iu_client.c
@@ -461,6 +461,12 @@ int ranap_iu_tx(struct msgb *msg_nas, uint8_t sapi)
 	struct msgb *msg;
 	struct osmo_scu_prim *prim;
 
+	if (!uectx) {
+		LOGPIU(LOGL_ERROR, "Discarding to-be-transmitted L3 Message as RANAP DT with unset dst SCCP conn_id!\n",
+		       uectx->conn_id);
+		return -ENOTCONN;
+	}
+
 	LOGPIU(LOGL_INFO, "Transmitting L3 Message as RANAP DT (SCCP conn_id %u)\n",
 	       uectx->conn_id);
 

commit fd410172bfba4812e10aa1b62f8b332450dde785

iu_client: Fix incorrect access to null pointer
Fixes: adec82f3b1c725e8a70d327b9592940fbdeaaff7
Closes: Coverity CID#243765
Related: OS#5398
Change-Id: Ib435b053241c294619c3478fe3b6450d8d181888

src/iu_client.c
@@ -462,8 +462,7 @@ int ranap_iu_tx(struct msgb *msg_nas, uint8_t sapi)
 	struct osmo_scu_prim *prim;
 
 	if (!uectx) {
-		LOGPIU(LOGL_ERROR, "Discarding to-be-transmitted L3 Message as RANAP DT with unset dst SCCP conn_id!\n",
-		       uectx->conn_id);
+		LOGPIU(LOGL_ERROR, "Discarding to-be-transmitted L3 Message as RANAP DT with unset dst SCCP conn_id!\n");
 		return -ENOTCONN;
 	}
 